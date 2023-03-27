a1f334752ef7480aabfa1dab5a5986d76ca83527
zhyzneuski86 <1293728@gmail.com>
2023-03-27 19:47:13 +0300

    add Todolist.tsx

---
+++ src/Todolist.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+
+export const Todolist = () => {
+
+    return  (
+    <div className="todolist">
+        <h3>What to learn</h3>
+        <div>
+            <input/>
+            <button>+</button>
+        </div>
+        <ul>
+            <li>
+                <input type="checkbox" checked={true}/>
+                <span>HTML&CSS</span>
+            </li>
+            <li>
+                <input type="checkbox" checked={true}/>
+                <span>JS</span>
+            </li>
+            <li>
+                <input type="checkbox" checked={false}/>
+                <span>React</span>
+            </li>
+        </ul>
+        <div>
+            <button>All</button>
+            <button>Active</button>
+            <button>Completed</button>
+        </div>
+    </div>
+    )
+}
+
+
